9427445c31cdfb1f7842287521f511e54a40ffa1
RomuaIdo <viniciusromualdo24@gmail.com>
2026-03-09 17:57:02 -0300

    Initial Commit

---
+++ src/script.py
@@ -0,0 +1,89 @@
+import requests
+import json
+import os
+from collections import Counter
+
+TOKEN = os.getenv("GH_TOKEN")
+USER = "RomuaIdo"
+
+if not TOKEN:
+    print("ERRO: Token não encontrado!")
+    exit(1)
+
+headers = {"Authorization": f"Bearer {TOKEN}"}
+print(f"Fetching data of {USER}...")
+
+url_user = f"https://api.github.com/users/{USER}"
+user_data = requests.get(url_user, headers=headers).json()
+followers = user_data.get("followers", 0)
+
+url_repos = f"https://api.github.com/users/{USER}/repos?per_page=100&sort=pushed"
+repos_data = requests.get(url_repos, headers=headers)
+
+if repos_data.status_code != 200:
+    print(f"ERRO na API: {repos_data.text}")
+    exit(1)
+
+repos = repos_data.json()
+
+stars_num = sum(repo.get('stargazers_count', 0) for repo in repos if isinstance(repo, dict))
+projects_num = len([repo for repo in repos if isinstance(repo, dict)])
+
+forks_num = sum(repo.get('forks_count', 0) for repo in repos if isinstance(repo, dict))
+issues_num = sum(repo.get('open_issues_count', 0) for repo in repos if isinstance(repo, dict))
+size_kb = sum(repo.get('size', 0) for repo in repos if isinstance(repo, dict))
+size_mb = round(size_kb / 1024, 2)
+
+last_project = repos[0].get('name', 'None') if repos else 'None'
+
+
+main_project = "None"
+more_stars = -1
+languages = {}
+
+for repo in repos:
+    if isinstance(repo, dict):
+        stars = repo.get('stargazers_count', 0)
+        if stars > more_stars:
+            more_stars = stars
+            main_project = repo.get('name', 'None')
+
+        languages_url = repo.get('languages_url')    
+        if languages_url:
+            langs_response = requests.get(languages_url, headers=headers)
+            
+            if langs_response.status_code == 200:
+                repo_langs = langs_response.json()
+                
+                for lang, bytes_count in repo_langs.items():
+                    languages[lang] = languages.get(lang, 0) + bytes_count
+
+
+top3_lang = Counter(languages).most_common(3)
+top3_lang_str = [lang for lang, count in top3_lang]
+while len(top3_lang_str) < 3:
+    top3_lang_str.append("None")
+
+top3_lang_json = {
+    "top1": top3_lang_str[0],
+    "top2": top3_lang_str[1],
+    "top3": top3_lang_str[2]
+}
+
+gh_data = {
+    "user": USER,
+    "followers": followers,
+    "stars_num": stars_num,
+    "projects_num": projects_num,
+    "forks_num": forks_num,
+    "issues_open": issues_num,
+    "size_mb": size_mb,
+    "main_languages": top3_lang_json,
+    "main_project": main_project,
+    "last_project": last_project
+}
+
+with open("../status.json", "w", encoding="utf-8") as file:
+    json.dump(gh_data, file, ensure_ascii=False, indent=2)
+
+print("status.json generated successfully!")
